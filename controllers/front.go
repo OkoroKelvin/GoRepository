5dedefe8d3389a84d88f98a741e7f14eaeb5196a
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2022-01-21 04:36:12 +0100

    Loops in Go

--- controllers/front.go
+++ controllers/front.go
@@ -4,7 +4,6 @@ import "net/http"
 
 func RegisterController() {
 	uc := newUserController()
-
 	http.Handle("/users", *uc)
 	http.Handle("/users/", *uc)
 }
--- slice/Slice.go
+++ slice/Slice.go
@@ -10,5 +10,23 @@ func main() {
 	s2 := slice[1:]
 	s3 := slice[:2]
 	s4 := slice[1:2]
+	s2[0] = 1000
+
+	fmt.Printf("slice %v %T \n", s2, s2)
 	fmt.Println(s2, s3, s4)
+
+	arr := [4]int{1, 2, 3, 4}
+
+	sArr := arr[:]
+	sArr[1] = 20
+	fmt.Println(arr)
+	fmt.Println(sArr)
+	fmt.Printf("%T\n", arr)
+	fmt.Printf("%T\n", sArr)
+	sArr = append(sArr, 2)
+	fmt.Printf("%T\n", sArr)
+
+	fmt.Println(sArr)
+	fmt.Println(arr)
+
 }
