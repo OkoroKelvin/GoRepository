9e27845ce96f27b574415d370968cd059a64b94c
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2022-01-24 02:09:12 +0100

    Go Switches statement

---
+++ switch/Switch.go
@@ -0,0 +1,21 @@
+package main
+
+type HTTPRequest struct {
+	Method string
+}
+
+func main() {
+	r := HTTPRequest{Method: "DELETE"}
+	switch r.Method {
+	case "GET":
+		println("Get request")
+	case "DELETE":
+		println("Delete request")
+	case "POST":
+		println("Post request")
+	case "PUT":
+		println("Put request")
+	default:
+		println("Unhandled method")
+	}
+}
